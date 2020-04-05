8bf05ed33029f489b07c9946768fbe03a5e51cd1
Bernardo Moderno <bernardo.moderno@gmail.com>
2020-04-05 12:55:10 -0300

    Rotas de alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,7 @@
-import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Ana',
-      sobrenome: 'Sa',
-      email: 'ana@gmail.com',
-      idade: 20,
-      peso: 50,
-      altura: 1.59,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
